4ababd27d0d0e9062ff4086690e8d87538ca949f
schimitel <liviaschimiteloficial@gmail.com>
2023-07-30 15:53:39 -0300

    adicionado + funcoes de js

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log ("rodando o sistema de gerenciamento de jogos")
+console.log ("rodando o sistema de gerenciamento de jogos");
+function mostra (frase) {
+    document.write(frase)
+}
+
+var nome = prompt ("Qual o seu nome?");
+var idade = parseInt(prompt("Qual a sua idade?"));
+
