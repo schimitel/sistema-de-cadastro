b1789735757fb278cf4f03602e91eada9641b19d
schimitel <liviaschimiteloficial@gmail.com>
2023-07-30 16:28:39 -0300

    voltando quando linkamos o index a app.js

--- app.js
+++ app.js
@@ -1,8 +1,1 @@
-console.log ("rodando o sistema de gerenciamento de jogos");
-function mostra (frase) {
-    document.write(frase)
-}
-
-var nome = prompt ("Qual o seu nome?");
-var idade = parseInt(prompt("Qual a sua idade?"));
-
+console.log ("rodando o sistema de gerenciamento de jogos")
